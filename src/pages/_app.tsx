31eafaf1595b606787bf466ceef7ad8c8345ee10
Arjun Ramanathan <arjun.ramanathan@affinity.co>
2025-02-11 11:50:50 -0800

    Fixing errors for prop types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 // src/pages/_app.tsx
 import "../styles/globals.css";
+import type { AppProps } from "next/app";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Navbar />
